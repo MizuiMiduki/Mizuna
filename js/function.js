bb6109003c52fd5f5e51c73929354b98b7e11ef3
MizuiMiduki <6161.k.iga@gmail.com>
2024-02-02 17:09:50 +0900

    R3.0-0.1.1

--- js/function.js
+++ js/function.js
@@ -1,24 +1,16 @@
 // アカウント認証
 const add_account = function (address, session_ID, Mizuna_host_address) {
-    window.open ("https://" + address + "/miauth/" + session_ID + "?name=Mizuna&callback=http://" + Mizuna_host_address + "&permission=write:notes","_blank")
+    location.assign("https://" + address + "/miauth/" + session_ID + "?name=Mizuna&callback=https://" + Mizuna_host_address + "&permission=write:notes", "_blank")
 };
 
 // アカウント情報取得
 const get_account_data = function (address, session_ID) {
-    $.ajax({
-        type: 'POST',
-        url: 'https://' + address + '/api/miauth/' + session_ID + '/check',
-        async: false,
-        contentType: 'application/json',
-        dataType: 'json',
-        scriptCharset: 'utf-8',
-        success: function (data) {
-            return data;
-        },
-        error: function () {
-            return "error";
-        }
-
-    });
-    doAnything();
+    let url = 'https://' + address + '/api/miauth/' + session_ID + '/check';
+    fetch(url, {
+        method: 'POST'
+    })
+        .then(response => response.text())
+        .then(data => {
+            console.log(data) 
+        });
 }
--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
 $(function () {
-    // Mizunaをホストしているページのホスト名
-    var Mizuna_host_address = location.hostname
-    // アカウント情報を取得するMisskeyサーバーのアドレス
-    var address = "misskey.io";
-    // セッションID
-    var session_ID = crypto.randomUUID();
-
     document.getElementById("get_account").addEventListener('click', () => {
+        // Mizunaをホストしているページのホスト名
+        var Mizuna_host_address = location.hostname
+        // アカウント情報を取得するMisskeyサーバーのアドレス
+        var address = "misskey.io";
+        // セッションID
+        var session_ID = crypto.randomUUID();
         add_account(address, session_ID, Mizuna_host_address)
     });
+
+
     document.getElementById("get_data").addEventListener('click', () => {
+        // アカウント情報を取得するMisskeyサーバーのアドレス
+        var address = "misskey.io";
+        // セッションIDを取得
+        session_ID = location.search.replace("?session=", "")
         let data = get_account_data(address, session_ID)
-        console.log(data)
+        // console.log(data)
     });
 });
