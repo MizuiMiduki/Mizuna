d880932fbf4975b7176b4df6c4028cb3270d4596
MizuiMiduki <6161.k.iga@gmail.com>
2024-02-08 22:49:01 +0900

    R3.0-0.1.3

--- js/function.js
+++ js/function.js
@@ -6,11 +6,34 @@ const add_account = function (address, session_ID, Mizuna_host_address) {
 // アカウント情報取得
 const get_account_data = function (address, session_ID) {
     let url = 'https://' + address + '/api/miauth/' + session_ID + '/check';
-        const request = new XMLHttpRequest();
-        request.open("POST", url, false); // `false` で同期リクエストになる
-        request.send(null);
-        if (request.status === 200) {
-            return request.responseText;
-        }
 
-}
+    const request = new XMLHttpRequest();
+    request.open("POST", url, false);
+    request.send(null);
+    if (request.status === 200) {
+        return request.responseText;
+    }
+};
+
+// indexedDBに登録する
+const add_indexeddb = function (get_user_data, address) {
+    let dbName = ["token_db", ""]
+
+    for (let i = 0; i < dbName.length; i++) {
+        var openReq = indexedDB.open(dbName[i]);
+
+        openReq.onupgradeneeded = function (event) {
+            console.log('db upgrade');
+        }
+        openReq.onsuccess = function (event) {
+            console.log('db open success');
+            var db = event.target.result;
+            // 接続を解除する
+            db.close();
+        }
+        openReq.onerror = function (event) {
+            console.log('db open error');
+            return "db_error"
+        }
+    }
+};
--- js/index.js
+++ js/index.js
@@ -18,13 +18,14 @@ $(function () {
         // サーバーからデータを取得
         let data = get_account_data(address, session_ID)
         let get_user_data = JSON.parse(data)
-
+        console.log(get_user_data)
+        
         if (get_user_data.ok == false) {
             // 取得できなかったとき
-            console.warn("取得できませんでした")
+            console.warn("アカウントデータを取得できませんでした")
         } else {
-            console.log(get_user_data)
-            const tr = db.transaction([storeName], "readwrite");
+            // indexedDBに登録
+            add_indexeddb(get_user_data, address)
         }
     });
 });
