79252b279e405ab385b6bb8227b693a756458e30
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2024-07-20 22:59:14 +0900

    キーボードショートカットを追加
    - ctl + alt + P : 投稿範囲パブリック
    - ctl + alt + H : 投稿範囲ホーム
    - ctl + alt + F : 投稿範囲フォロワー
    - ctl + alt + W : CW
    - ctl + alt + M : 末尾に"from Mizuna"を付与
    - ctl + alt + C : フォームクリア
    - ctl + alt +Enter : 投稿

--- js/function/keybordshortcut.js
+++ js/function/keybordshortcut.js
@@ -1,6 +1,35 @@
 // コントロール+エンターキーで投稿
 document.addEventListener('keydown', event => {
-    if (event.ctrlKey && event.altKey && event.key === 'Enter') {
-        $(".note_submit").click();
+    if (event.ctrlKey && event.altKey) {
+        switch (event.key) {
+            case 'Enter':
+                // 投稿
+                $(".note_submit").click();
+                break;
+            case 'p':
+                // 投稿範囲パブリック
+                $("#visibility_public").click();
+                break;
+            case 'h':
+                // 投稿範囲ホーム
+                $("#visibility_home").click();
+                break;
+            case 'f':
+                // 投稿範囲フォロワー
+                $("#visibility_followers").click();
+                break;
+            case 'w':
+                // CW
+                $("#cw").click();
+                break;
+            case 'm':
+                // from Mizuna
+                $("#note_end_mizuna_checkbox").click();
+                break;
+            case 'c':
+                // フォームクリア
+                $("#form_clear_button").click();
+                break;
+        }
     }
 });
