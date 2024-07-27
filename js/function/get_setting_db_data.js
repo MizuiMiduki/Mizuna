ff7aea41d70da3570bd38e198d9a6aecbfb9bf1f
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2024-07-28 01:53:31 +0900

    バグ対策でユーザー設定が空だった際にダミーを適用するように変更

--- js/function/get_setting_db_data.js
+++ js/function/get_setting_db_data.js
@@ -5,23 +5,42 @@ const get_setting_db_data = function () {
 const user_setting = function () {
     get_setting_db_data()
         .then(get_db_result => {
-            var setting_data = get_db_result[0]
-
-            user_options = {
-                // 表示するユーザーのID(初期値:1)
-                "select_user": setting_data.select_user ?? 1,
-                // デフォルトの投稿範囲(初期値:1 / 1:public, 2:home, 3:followers)
-                "default_visibility": setting_data.default_visibility ?? 1,
-                // ノートの最後に"from Mizuna"をデフォルトで付けるか(初期値:false)
-                "is_note_end_mizuna": setting_data.is_note_end_mizuna ?? false,
-                // 自分のアイコンを表示するか(初期値:true)
-                "is_visible_icon": setting_data.is_visible_icon ?? true,
-                // UIモード(初期値:1)
-                "ui_mode": setting_data.ui_mode ?? 1,
-                // ダークモードを有効にするか(初期値:1)
-                "is_darkmode": setting_data.is_darkmode ?? 1,
-                // アイコンカラーを元にUIカラーを変更するか(初期値:false)
-                "is_pick_theme_color": setting_data.is_pick_theme_color ?? false,
+            var setting_data = get_db_result[0];
+            if (setting_data) {
+                user_options = {
+                    // 表示するユーザーのID(初期値:1)
+                    "select_user": setting_data.select_user ?? 1,
+                    // デフォルトの投稿範囲(初期値:1 / 1:public, 2:home, 3:followers)
+                    "default_visibility": setting_data.default_visibility ?? 1,
+                    // ノートの最後に"from Mizuna"をデフォルトで付けるか(初期値:false)
+                    "is_note_end_mizuna": setting_data.is_note_end_mizuna ?? false,
+                    // 自分のアイコンを表示するか(初期値:true)
+                    "is_visible_icon": setting_data.is_visible_icon ?? true,
+                    // UIモード(初期値:1)
+                    "ui_mode": setting_data.ui_mode ?? 1,
+                    // ダークモードを有効にするか(初期値:1)
+                    "is_darkmode": setting_data.is_darkmode ?? 1,
+                    // アイコンカラーを元にUIカラーを変更するか(初期値:false)
+                    "is_pick_theme_color": setting_data.is_pick_theme_color ?? false,
+                }
+            } else {
+                // ユーザー設定が存在しなかった場合にダミーを適用する
+                user_options = {
+                    // 表示するユーザーのID
+                    "select_user": 1,
+                    // デフォルトの投稿範囲
+                    "default_visibility": 1,
+                    // ノートの最後に"from Mizuna"をデフォルトで付けるか
+                    "is_note_end_mizuna": false,
+                    // 自分のアイコンを表示するか
+                    "is_visible_icon": true,
+                    // UIモード
+                    "ui_mode": 1,
+                    // ダークモードを有効にするか
+                    "is_darkmode": 1,
+                    // アイコンカラーを元にUIカラーを変更するか
+                    "is_pick_theme_color": false,
+                }
             }
         })
 };
