7b9b7ef0212e3700924f747d0348562e73b5116e
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2024-08-03 01:14:57 +0900

    アカウント切り替えに対応

--- js/servise/generate_menu_service.js
+++ js/servise/generate_menu_service.js
@@ -1,8 +1,10 @@
 const generate_menu_service = function () {
     $(".main_column").load("/parts/change_account.html", function () {
+        // $(".main_column").load("/parts/.html", function () {
         $("#loading_anime_area").remove();
         // アカウント一覧を表示
         generate_account_list()
+        // });
     });
 }
 
@@ -49,4 +51,24 @@ $(document).on('click', '.back_inputform_floating_button', function () {
         // 戻るボタンを削除
         $(".back_inputform_floating_button").remove();
     });
+});
+
+$(document).on('click', '#account_card', function () {
+    let select_user_id = $(this).data('id');
+    db.setting.bulkUpdate([
+        {
+            key: 1,
+            changes: {
+                select_user: select_user_id,
+            }
+        },
+    ]).then(function () {
+        get_user_db_data([select_user_id])
+            .then(get_db_result => {
+                // フッター表示
+                user_data = get_db_result[0]
+                set_user_text(user_data)
+                toastr["success"]('アカウントを切り替えました');
+            })
+    })
 });
