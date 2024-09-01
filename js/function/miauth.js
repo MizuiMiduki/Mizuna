dd25a81cf2dcf8a19f0d7ecabaf1a6431497d58a
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2024-09-01 21:55:37 +0900

    MiAuthでの登録時に使用する名前を"Mizuna3.0"から"Mizuna"に変更

--- js/function/miauth.js
+++ js/function/miauth.js
@@ -2,5 +2,5 @@
 var miauth = function (address, session_ID, Mizuna_host_address) {
     $("#add_account_server_button").prop("disabled", true);
 
-    location.assign("https://" + address + "/miauth/" + session_ID + "?name=Mizuna3.0&callback=https://" + Mizuna_host_address + "&permission=write:notes,read:account", "_blank")
+    location.assign("https://" + address + "/miauth/" + session_ID + "?name=Mizuna&callback=https://" + Mizuna_host_address + "&permission=write:notes,read:account", "_blank")
 };
