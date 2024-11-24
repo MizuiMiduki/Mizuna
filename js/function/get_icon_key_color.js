4893e747b3a064e6f567f15d4f042040ed964b02
瑞井 箕月 <6161.k.iga@gmail.com>
2024-11-24 22:22:52 +0900

    アイコンカラーからキーカラーを抽出する機能の修正

--- js/function/get_icon_key_color.js
+++ js/function/get_icon_key_color.js
@@ -2,20 +2,24 @@
 const img = document.getElementById('menu_icon');
 
 const get_icon_key_color = function () {
-    if (img.src.endsWith('/icon.png')) {
-        document.documentElement.style.setProperty('--main_color', 'lightblue');
-        document.documentElement.style.setProperty('--button_text_color', 'black');
-        return;
-    }
+    if (1 == user_options.is_darkmode) {
+        if (img.src.endsWith('/icon.png')) {
+            document.documentElement.style.setProperty('--main_color', 'lightblue');
+            document.documentElement.style.setProperty('--text_color: black;', 'black');
+            document.documentElement.style.setProperty('--button_text_color', 'black');
+            return;
+        }
 
-    Vibrant.from(img).getPalette()
-        .then((palette) => {
-            document.documentElement.style.setProperty('--main_color', `rgb(${palette.LightVibrant.rgb.join(', ')})`);
-            document.documentElement.style.setProperty('--button_text_color', `rgb(${palette.DarkVibrant.rgb.join(', ')})`);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+        Vibrant.from(img).getPalette()
+            .then((palette) => {
+                document.documentElement.style.setProperty('--main_color', `rgb(${palette.LightVibrant.rgb.join(', ')})`);
+                document.documentElement.style.setProperty('--text_color: black;', `rgb(${palette.DarkVibrant.rgb.join(', ')})`);
+                document.documentElement.style.setProperty('--button_text_color', `rgb(${palette.DarkVibrant.rgb.join(', ')})`);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
 }
 
 img.onload = function () {
