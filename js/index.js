a0a4d25d3b0b47b459f65d7d3d79438dd169b915
瑞井 箕月 <6161.k.iga@gmail.com>
2024-12-01 00:07:45 +0900

    #103 の実装

--- js/index.js
+++ js/index.js
@@ -32,6 +32,10 @@ db.version(1).stores({
     account: "++id, token, name, username, address, avatarurl, add_mizuna_versinon",
     setting: '++id, select_user, default_visibility, is_note_end_mizuna, is_visible_icon, ui_mode, is_darkmode, is_pick_theme_color',
 });
+db.version(2).stores({
+    account: "++id, token, name, username, address, avatarurl, add_mizuna_versinon",
+    setting: '++id, select_user, default_visibility, is_note_end_mizuna, is_visible_icon, ui_mode, is_darkmode, is_pick_theme_color, latest_theme_color, is_check_releasenote'
+});
 
 // ユーザー設定を読み込み
 $.getScript("/js/function/get_setting_db_data.js", function () {
