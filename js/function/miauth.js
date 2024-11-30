03c97eab70fb47ede474bf754a123cb2d68a8302
瑞井 箕月 <6161.k.iga@gmail.com>
2024-11-30 16:09:41 +0900

    アカウント認証中にブラウザバックした場合に、ボタンが無効化されたままになる不具合の修正

--- js/function/miauth.js
+++ js/function/miauth.js
@@ -3,4 +3,5 @@ var miauth = function (address, session_ID, Mizuna_host_address) {
     $("#add_account_server_button").prop("disabled", true);
 
     location.assign("https://" + address + "/miauth/" + session_ID + "?name=Mizuna&callback=https://" + Mizuna_host_address + "&permission=write:notes,read:account", "_blank")
+    $("#add_account_server_button").prop("disabled", false);
 };
