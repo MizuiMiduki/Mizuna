62fed7f54c51d0e21db45043e123ba02c7341c6e
瑞井 箕月 <6161.k.iga@gmail.com>
2024-11-30 16:39:34 +0900

    #82 の実装

--- js/function/miauth.js
+++ js/function/miauth.js
@@ -2,6 +2,16 @@
 var miauth = function (address, session_ID, Mizuna_host_address) {
     $("#add_account_server_button").prop("disabled", true);
 
-    location.assign("https://" + address + "/miauth/" + session_ID + "?name=Mizuna&callback=https://" + Mizuna_host_address + "&permission=write:notes,read:account", "_blank")
+    const check_url = `https://${Mizuna_host_address}/php/check_url.php?url=https://${address}/`;
+    fetch(check_url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 'success') {
+                location.assign("https://" + address + "/miauth/" + session_ID + "?name=Mizuna&callback=https://" + Mizuna_host_address + "&permission=write:notes,read:account", "_blank")
+            } else {
+                toastr["error"]("アドレスのサーバーは見つかりませんでした")
+            }
+        })
+
     $("#add_account_server_button").prop("disabled", false);
 };
