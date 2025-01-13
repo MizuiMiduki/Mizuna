0054a63e29d38d096d0a0f9026400cd75391bccb
瑞井 箕月 <6161.k.iga@gmail.com>
2025-01-14 00:51:22 +0900

    シェア時のURLを製品サイトに変更

--- js/option/mizuna_option.js
+++ js/option/mizuna_option.js
@@ -7,5 +7,5 @@ mizuna_options = {
 mizuna_share = {
     title: 'Mizuna - ノート専用Misskeyクライアント',
     text: 'Mizunaは機能をノート送信のみにすることで通信量削減に重点を置いたMisskeyクライアントです',
-    url: mizuna_options.mizuna_address,
+    url: "https://mizuna-project.blossomsarchive.com/",
 }
