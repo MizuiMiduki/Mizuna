0804c415c0e959e6da6d44d1d86558a1c2248497
瑞井 箕月 <6161.k.iga@gmail.com>
2025-01-21 00:14:50 +0900

    一部環境でマーキーエリアが表示されないことがある問題を修正

--- js/function/news_marquee.js
+++ js/function/news_marquee.js
@@ -1,12 +1,33 @@
-const $marqueeContainer = $('.marquee-container');
-const $marqueeContent = $('.marquee-content');
-const $closeIcon = $('.close-icon');
+const marqueeContainer = document.querySelector('.marquee-container');
+const marqueeContent = document.querySelector('.marquee-content');
+const closeIcon = document.querySelector('.close-icon');
+
+marqueeContainer.style.display = 'none';
 
 if (mizuna_options.marquee_text) {
-    $marqueeContent.text(mizuna_options.marquee_text);
-    $marqueeContainer.slideToggle();
+    marqueeContent.textContent = mizuna_options.marquee_text;
+    marqueeContainer.style.display = 'block';
+    marqueeContainer.style.opacity = 0;
+    let opacity = 0;
+    const fadeInInterval = setInterval(() => {
+        if (opacity < 1) {
+            opacity += 0.1;
+            marqueeContainer.style.opacity = opacity;
+        } else {
+            clearInterval(fadeInInterval);
+        }
+    }, 30);
 }
 
-$closeIcon.on('click', () => {
-    $marqueeContainer.slideToggle();
+closeIcon.addEventListener('click', () => {
+    let opacity = 1;
+    const fadeOutInterval = setInterval(() => {
+        if (opacity > 0) {
+            opacity -= 0.1;
+            marqueeContainer.style.opacity = opacity;
+        } else {
+            clearInterval(fadeOutInterval);
+            marqueeContainer.style.display = 'none';
+        }
+    }, 30);
 });
