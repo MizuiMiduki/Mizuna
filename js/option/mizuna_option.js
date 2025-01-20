3b9486fae0faa32b16dd02d893fa71ffafb09a2c
瑞井 箕月 <6161.k.iga@gmail.com>
2025-01-20 22:56:19 +0900

    お知らせマーキーエリアを実装

--- js/option/mizuna_option.js
+++ js/option/mizuna_option.js
@@ -1,11 +1,12 @@
 mizuna_options = {
-    mizuna_version: "3.5.2",
-    mizuna_address: "https://" + location.hostname,
+    mizuna_version: '3.5.2',
+    mizuna_address: 'https://' + location.hostname,
     maintenance: false,
+    marquee_text: '',
 }
 
 mizuna_share = {
     title: 'Mizuna - ノート専用Misskeyクライアント',
     text: 'Mizunaは機能をノート送信のみにすることで通信量削減に重点を置いたMisskeyクライアントです',
-    url: "https://mizuna-project.blossomsarchive.com/",
+    url: 'https://mizuna-project.blossomsarchive.com/',
 }
