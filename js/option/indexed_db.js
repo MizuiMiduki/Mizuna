76d255ef219395ccab76a4cbf7c77be7d06efe46
瑞井 箕月 <6161.k.iga@gmail.com>
2026-06-14 22:15:50 +0900

    Update indexed_db.js

--- js/option/indexed_db.js
+++ js/option/indexed_db.js
@@ -9,12 +9,10 @@ db.version(1).stores({
 
 // v2
 db.version(2).stores({
-    account: "++id, token, name, username, address, avatarurl, add_mizuna_versinon",
     setting: '++id, select_user, default_visibility, is_note_end_mizuna, is_visible_icon, ui_mode, is_darkmode, is_pick_theme_color, latest_theme_color, is_check_releasenote'
 });
 
 // v3
 db.version(3).stores({
-    account: "++id, token, name, username, address, avatarurl, add_mizuna_versinon",
     setting: '++id, select_user, default_visibility, is_note_end_mizuna, is_visible_icon, ui_mode, is_darkmode, is_pick_theme_color, latest_theme_color, is_check_releasenote, language'
 });
